991c47b63c22c2f897120bea00ddf1d5bd83f179
xiaoji <wangruoningzhenshuai@gmail.com>
2020-05-14 00:58:15 +0800

    shiro以及配置

---
+++ src/main/java/com/xiaoji/noteback/realm/CustomRealm.java
@@ -0,0 +1,34 @@
+package com.xiaoji.noteback.realm;
+
+import com.xiaoji.noteback.pojo.User;
+import com.xiaoji.noteback.service.UserService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomRealm extends AuthorizingRealm {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String userName = authenticationToken.getPrincipal().toString();
+        User user = userService.getUserByUsername(userName);
+        String password = user.getPassword();
+        String salt = user.getSalt();
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(user,password, ByteSource.Util.bytes(salt),getName());
+        return simpleAuthenticationInfo;
+    }
+}
